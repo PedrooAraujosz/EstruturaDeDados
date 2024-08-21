4ab047c6d51b1c4a7a4c7d3ecd32d441d15c3441
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-08-21 16:19:52 -0300

    aterações list e Fila

--- EstruturasDeDados/Program.cs
+++ EstruturasDeDados/Program.cs
@@ -1,9 +1,43 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("my firts branch ok?");
-        Console.WriteLine("Trabalhando com Git\\Github");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+
+        List<string> frutas = new List<string>();
+
+        frutas.Add("Maçã");
+        frutas.Add("Uva");
+        frutas.Add("Pera");
+        frutas.Add("Kiwi");
+
+        Console.WriteLine("Imprimindo Minha Lista: ");
+        foreach (string fruit in frutas)
+        { Console.WriteLine(fruit); }
+
+        Console.WriteLine();
+        Console.WriteLine("===========================");
+
+        //Trabalhando com fila 
+        /*Criar fila (Queue)*/
+
+        Queue<string> filaBanco = new Queue<string>();
+
+        filaBanco.Enqueue("Pedro");
+        filaBanco.Enqueue("luiz");
+        filaBanco.Enqueue("Melissa");
+        filaBanco.Enqueue("Wilian");
+
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+        filaBanco.Dequeue();
+
+        Console.WriteLine();
+        Console.WriteLine("===========================");
+
+
     }
 }
