e26ff74f93128ef598297a13118474d4f0152e12
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-08-21 10:39:03 -0300

    TRabalhando com branch.teste2

--- EstruturasDeDados/Program.cs
+++ EstruturasDeDados/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("my firts branch ok?");
+        Console.WriteLine("Trabalhando com Git\\Github");
+        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
     }
 }
